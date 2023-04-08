6b49881ca47347707f94aa597de5cb9749554e0a
Ryan <ryan.lee8867@outlook.com>
2023-04-07 18:36:40 -0700

    Adds an event listener to add functionality to the new note button that adds text area, save and cancel buttons if they are not there. Clears text area when pressed while text area, save and cancel buttons are there

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,11 @@
-//Dark & Light Theme Toggle
 const body = document.querySelector('body');
 const darkModeButton = document.querySelector('.darkModeButton');
+const cancelButton = document.querySelector(".cancelButton");
+const saveButton = document.querySelector(".saveButton");
+const textArea = document.querySelector("textarea");
+const newNoteButton = document.querySelector(".newNoteButton");
 
+//Dark & Light Theme Toggle
 darkModeButton.addEventListener('click', () => {
   body.classList.toggle('darkMode');
   if (body.classList.contains('darkMode')) {
@@ -12,13 +16,18 @@ darkModeButton.addEventListener('click', () => {
 });
 
 //Cancel Button
-const cancelButton = document.querySelector(".cancelButton");
-const saveButton = document.querySelector(".saveButton");
-const textArea = document.querySelector("textarea");
-
 cancelButton.addEventListener("click", () => {
   // hide text area, save button, and cancel button
   textArea.style.display = "none";
   saveButton.style.display = "none";
   cancelButton.style.display = "none";
 });
+
+//New Note Button
+newNoteButton.addEventListener('click', () => {
+    textArea.style.display = 'block';
+    textArea.value = '';
+    saveButton.style.display = 'block';
+    cancelButton.style.display = 'block';
+});
+
